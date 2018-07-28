43a2ce06916b46e734ae37f7f717f2c0f97a1fb8
Stan Bondi <stan@fixate.it>
2018-07-28 22:15:12 +0200

    wip drawing board

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,33 @@
+extern crate opengl_graphics;
 extern crate piston;
 extern crate piston_window;
 extern crate sdl2_window;
 
-use piston::event_loop::{EventSettings, Events};
-use piston_window::{PistonWindow, WindowSettings};
+use piston::event_loop::*;
+use piston_window::*;
 use sdl2_window::Sdl2Window;
 
+mod app;
 mod board;
-mod renderer;
 
 const WINDOW_TITLE: &'static str = "Sudoku Solver";
 const WINDOW_SIZE: [u32; 2] = [800, 640];
 
 fn main() {
+  use opengl_graphics::GlGraphics;
+
   let mut window: PistonWindow<Sdl2Window> = WindowSettings::new(WINDOW_TITLE, WINDOW_SIZE)
     .exit_on_esc(true)
     .build()
     .unwrap();
 
-  let board = board::new();
-  let renderer = renderer::new();
+  let mut a = app::App::new();
 
+  let mut gl = GlGraphics::new(OpenGL::V3_2);
   let mut events = Events::new(EventSettings::new());
   while let Some(e) = events.next(&mut window) {
     if let Some(args) = e.render_args() {
-      renderer.render_ui(args)
+      a.render(&args, &mut gl);
     }
   }
 }
